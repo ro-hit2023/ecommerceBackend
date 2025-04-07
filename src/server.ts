945e9f427f0d1f4550acdc424d19660c68d477a3
Rohit Kumar <rhtkumar700@gmail.com>
2025-04-07 17:29:56 +0530

    taking pull from master

--- src/server.ts
+++ src/server.ts
@@ -12,22 +12,179 @@ import { uploadImage_Schema } from "./Validators/commonValidator";
 import { fileSizeCheck, upload } from "./utils/multer";
 import multer from "multer";
 import messages from "./custome_messages";
+import path from "path";
 // import fs from "node:fs"
-
 
 const app = express()
 app.use(express.json())
 env.config()
 app.use(status())
 
-//db connection
-// mongoose.connect('mongodb://localhost:27017/Kart_clone');
-  
-//   const db = mongoose.connection;
-//   db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-//   db.once('open', async () => {
-//     console.log('Connected to MongoDB');
-// });
+app.use(express.static(path.join(__dirname + '/public')));
+
+
+//---------------------UI--------------------------------//
+
+//Public----
+app.get('/vendoradd', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/vendoradd.html'));
+});
+// login addmin
+app.get('/login', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/login.html'));
+});
+//vendor listing
+app.get('/vendor', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/allvendor.html'));
+});
+//vendor view
+app.get('/vendorView', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/vendorView.html'));
+});
+//user view
+app.get('/userView', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/userView.html'));
+});
+// vendor edit
+app.get('/vendoredit', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/vendoredit.html'));
+});
+// User Edit
+app.get('/useredit', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/useredit.html'));
+});
+//User listing
+app.get('/user', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/user.html'));
+});
+//category listing
+app.get('/categories', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/categories.html'));
+});
+// menu Listing
+app.get('/menu', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/menu.html'));
+});
+//add menu
+app.get('/addmenu', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/menuAdd.html'));
+});
+// add categories
+app.get('/addcategory', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/categoriesAdd.html'));
+});
+// offer listing
+app.get('/offers', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/OfferListing.html'));
+});
+//banner list
+app.get('/bannerList', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/bannerList.html'));
+});
+//add banner
+app.get('/addbanner', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/addbanner.html'));
+});
+//edit banner
+app.get('/editBanner', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/editBanner.html'));
+});
+//notification single
+app.get('/singleNotification', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/singleNotification.html'));
+});
+//bulk notofication
+app.get('/bulkNotification', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/bulkNotification.html'));
+});
+//Add On
+app.get('/AddonListing', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/AddonListing.html'));
+});
+// FAQs
+app.get('/FA', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/Faq.html'));
+});
+app.get('/addOffers', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/addOffer.html'));
+});
+app.get('/sizeListing', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/sizeListing.html'));
+});
+app.get('/dashboard', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/dashboard.html'));
+});
+app.get('/app_version', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/app_version.html'));
+});
+app.get('/settings', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/changepassword.html'));
+});
+// FAQs
+app.get('/FAQs', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/Faq.html'));
+});
+app.get('/addFAQ', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/addFAQ.html'));
+});
+app.get('/EditFaq', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/editFaq.html'));
+});
+app.get('/termAndCondition', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/pizzaNpints_termAndCondition.html'));
+});
+
+app.get('/privacy', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/pizzaNpints_privacy.html'));
+});
+app.get('/complaints', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/complaints.html'));
+});
+//report Details
+app.get('/reportDetails', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/reportDetails.html'));
+});
+//Order Inprogress
+app.get('/orderProgress', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/orderProgress.html'));
+});
+app.get('/orderComplete', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/orderComplete.html'));
+});
+app.get('/orderCancel', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/orderCancel.html'));
+});
+app.get('/orderDetails', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/orderDetail.html'));
+});
+app.get('/review', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/review.html'));
+});
+app.get('/addonCategory', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/addonCategory.html'));
+});
+app.get('/vendortime', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/vendortime.html'));
+
+});
+app.get('/vendorEmployee', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/vendorEmployee.html'));
+});
+app.get('/vendorGallery', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/vendorGallery.html'));
+});
+app.get('/branch', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/branch.html'));
+});
+app.get('/orderPending', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/orderPending.html'));
+});
+app.get('/newCategoryadd', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/newCategoryadd.html'));
+});
+app.get('/test', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/test.html'));
+});
 
 
 
@@ -127,8 +284,6 @@ const multerErrorHandler = (err:Error, req:Request, res:any, next:NextFunction)=
 }
 
 apiRoute.use('/upload', validatorJoiSchema_query(uploadImage_Schema), upload.fields([{name:'Image', maxCount:1}]), multerErrorHandler, fileSizeCheck) 
-
-
 
 
 app.listen(process.env.PORT, ()=>{
